acc1b940ae0a8c035649c22101d06421b674be6e
KeziahMoselle <keziahmoselle@protonmail.com>
2018-02-08 22:41:07 +0100

    v0.0.2

--- renderer.js
+++ renderer.js
@@ -10,17 +10,18 @@
 
 // Var
 
-  // API URL
-  const apiUrl = (parameters) => {
-    return `https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=10&json=1&tags=${parameters}&pid=1`;
-  };
   // HTML elements
-  const body = document.getElementsByTagName('body'); // 
+  const root = document.getElementById('root'); // 
   const searchBar = document.getElementById('searchBar'); // Input type text
   const searchBtn = document.getElementById('searchBtn'); // Input type btn
   const loading = document.getElementById('loading'); // Display loading or not
   const container = document.getElementById('container'); // Container of images
   const themeBtn = document.getElementById('themeBtn'); // Checkbox for theme
+  const imgLimit = document.getElementById('imgLimit'); // Select for img limit
+  const selectCardLayout = document.getElementById('selectCardLayout'); // Select for Card Layout
+  // Value
+  var limit = 10;
+  var layout = 'm4';
   // Links
   const linkGelbooru = document.getElementById('linkGelbooru');
 
@@ -52,29 +53,128 @@
       window.close();
     });
 
-  // Permet de récupérer 10 images avec des tags
+
+  // INPUT AND HIT ENTER
   searchBar.onkeypress = (event) => {
     if (event.keyCode === 13)
     {
-      event.preventDefault();
-      if (searchBar.value === "")
-        return;
-      // On enlève la classe "hide" du loading pour qu'il s'affiche
-      while (container.firstChild) {
+      let tags = searchBar.value;
+      search(tags, limit, layout);
+    }
+  };
+
+  // BTN
+  searchBtn.addEventListener('click', () => {
+    let tags = searchBar.value;
+    search(tags, limit, layout);
+  });
+
+  // LIMIT VAR
+  imgLimit.addEventListener('change', () => {
+    limit = imgLimit.value;
+    console.log(limit);
+  });
+
+  // LAYOUT VAR
+  selectCardLayout.addEventListener('change', () => {
+    layout = selectCardLayout.value;
+    console.log(layout);
+  });
+
+  // Light & Dark Mode
+  themeBtn.addEventListener('change', () => {
+    if (themeBtn.checked)
+    { // Si la checkbox est check = on veut le dark mode
+      root.classList.add('light-mode');
+      console.log('Light mode !');
+    }
+    else
+    { // Si la checkbox n'est pas check on retire le dark mode
+      root.classList.remove('light-mode');
+      console.log('Dark mode !');
+    }
+  });
+
+  // Links
+  linkGelbooru.addEventListener('click', (event) => {
+    event.preventDefault();
+    shell.openExternal('https://gelbooru.com');
+  });
+  linkGithub.addEventListener('click', (event) => {
+    event.preventDefault();
+    shell.openExternal('https://github.com/KeziahMoselle/gelbooru-client');
+  });
+
+
+
+/**
+ * GET Request to Gelbooru and display results
+ * @param {string} tags 
+ * @param {number} limit 
+ */
+function search(tags, limit = 10, layout)
+{
+
+  if (searchBar.value === "")
+    {
+      // Empty cards
+      while (container.firstChild)
+      {
         container.removeChild(container.firstChild);
       }
+
+      // Display loading...
       loading.classList.remove('hide');
-      // Fetching
-        // On récupère la valeur des tags 
-        let tags = searchBar.value;
-        // On remplace les espaces par des '+'
+
+      // GET request
+      axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=${limit}&json=1&pid=1`, {})
+      .then((response) => {
+        // Display images
+        response.data.forEach(image => {
+            container.insertAdjacentHTML('beforeend', `<div class="col s12 ${layout}">
+              <div class="card">
+                <div class="card-image">
+                  <img src="${image.file_url}">
+                </div>
+                <div class="card-action">
+                  <a href="${image.source}">Source</a>
+                </div>
+              </div>
+            </div>`);
+        });
+
+        // Hide loading ...
+        loading.classList.add('hide');
+
+      }).catch((error) => {
+        console.log(error);
+        loading.classList.add('hide');
+        container.insertAdjacentHTML('afterbegin', `<div class="card-panel red white-text">
+        <span class="white-text">
+          ${error}
+        </span>
+      </div>`);
+      });
+    }
+    else
+    {
+      // Empty cards
+      while (container.firstChild)
+      {
+        container.removeChild(container.firstChild);
+      }
+
+      // Display loading ...
+      loading.classList.remove('hide');
+
+        // Tags replace space -> '+'
         tags.replace(/\s/g, '+');
-        // On exécute la requête
-        axios.get(apiUrl(tags), {})
+
+        // GET request
+        axios.get(`https://gelbooru.com/index.php?page=dapi&s=post&q=index&limit=${limit}&json=1&tags=${tags}&pid=1`, {})
         .then((response) => {
-          // On affiche les images
           response.data.forEach(image => {
-              container.insertAdjacentHTML('beforeend', `<div class="col s12 m4">
+              container.insertAdjacentHTML('beforeend', `<div class="col s12 ${layout}">
                 <div class="card">
                   <div class="card-image">
                     <img src="${image.file_url}">
@@ -85,8 +185,10 @@
                 </div>
               </div>`);
           });
-          // On enlève le loading
+
+          // Hide loading ...
           loading.classList.add('hide');
+
         }).catch((error) => {
           console.log(error);
           loading.classList.add('hide');
@@ -97,57 +199,5 @@
         </div>`);
         });
     }
-  };
 
-  // Permet de récupérer 10 images avec des tags grâce au btn
-  searchBtn.addEventListener('click', () => {
-    if (searchBar.value === "")
-        return;
-      // On enlève la classe "hide" du loading pour qu'il s'affiche
-      loading.classList.remove('hide');
-      // Fetching
-        // On récupère la valeur des tags 
-        let tags = searchBar.value;
-        // On remplace les espaces par des '+'
-        tags.replace(/\s/g, '+');
-        // On exécute la requête
-        axios.get(apiUrl(tags), {})
-        .then((response) => {
-          // On affiche les images
-          response.data.forEach(image => {
-              container.insertAdjacentHTML('beforeend', `<div class="col s12 m4">
-                <div class="card">
-                  <div class="card-image">
-                    <img src="${image.file_url}">
-                    <span class="card-title">Card Title</span>
-                  </div>
-                  <div class="card-action">
-                    <a href="${image.source}">Source</a>
-                  </div>
-                </div>
-              </div>`);
-          });
-          // On enlève le loading
-          loading.classList.add('hide');
-        }).catch((error) => {
-          console.log(error);
-        });
-  });
-
-  // Light & Dark Mode
-  themeBtn.addEventListener('change', () => {
-    if (this.checked)
-    { // Si la checkbox est check = on veut le dark mode
-      body.classList.add('light-mode');
-    }
-    else
-    { // Si la checkbox n'est pas check on retire le dark mode
-      body.classList.remove('light-mode');
-    }
-  });
-
-
-  // Links
-  linkGelbooru.addEventListener('click', (event) => {
-    shell.openExternal('https://gelbooru.com/');
-  });
+}
