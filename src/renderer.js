42a4f2351585320bc4bac00974859f877dadd3eb
KeziahMoselle <keziahmoselle@protonmail.com>
2018-03-26 16:59:41 +0200

    (v0.7.3) webm: Add loop

--- src/renderer.js
+++ src/renderer.js
@@ -446,7 +446,7 @@ function getResults(url)
           if (tags.includes('webm'))
           {
             container.insertAdjacentHTML('beforeend', `
-            <video class="responsive-video" controls>
+            <video class="responsive-video" controls loop>
               <source src="${image.file_url}" type="video/webm">
             </video>`)
           }
