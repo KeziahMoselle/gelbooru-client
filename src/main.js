32d936c4f0e2e8c2392f625220feede71421efca
Ame <vadimpere98@gmail.com>
2018-09-27 22:38:19 +0200

    Fixed main path

--- src/main.js
+++ src/main.js
@@ -33,7 +33,7 @@
       title: 'Gelbooru Client'
     });
     window.loadURL(url.format({
-      pathname: path.join(__dirname, './src/index.html'),
+      pathname: path.join(__dirname, './index.html'),
       protocol: 'file:',
       slashes: true
     }));
