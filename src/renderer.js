080ae213be58a7c7a1a8ff16e535485b099789dd
KeziahMoselle <keziahmoselle@protonmail.com>
2018-10-03 19:21:25 +0200

    Fix #24

--- src/renderer.js
+++ src/renderer.js
@@ -624,41 +624,36 @@ document.getElementById('darkColor').addEventListener('keypress', (event) => {
   }
 })
 
-// Enable / Disable Endless Scrolling
+// Enable endless scrolling if `true` at launch
 if (store.get('endless')) {
   // Check the checkbox at launch
   document.getElementById('checkboxEndlessScrolling').setAttribute('checked', 'checked')
   // Enable Endless Scrolling
-  windowElement.addEventListener('scroll', () => {
-    if (windowElement.scrollY >= documentElement.scrollHeight - windowElement.innerHeight - 0.1) {
-      let url = getUrl(tags, imgLimit, rating)
-      pid++
-      getResults(url + `&pid=${pid}`)
-      displayPid.innerHTML = `Page ${pid}`
-    }
-  })
+  windowElement.addEventListener('scroll', handleEndlessScrolling)
 }
 
 document.getElementById('checkboxEndlessScrolling').addEventListener('click', () => {
   if (store.get('endless')) {
     // If it's enabled, disable it
     store.set('endless', false)
+    windowElement.removeEventListener('scroll', handleEndlessScrolling)
     M.toast({ html: 'Disabled : Endless Scrolling' })
   } else {
-    // If it's disabled, enable it
     store.set('endless', true)
     // Enable Endless Scrolling
-    windowElement.addEventListener('scroll', () => {
-      if (windowElement.scrollY >= documentElement.scrollHeight - windowElement.innerHeight - 0.1) {
-        let url = getUrl(tags, imgLimit, rating)
-        pid++
-        getResults(url + `&pid=${pid}`)
-        displayPid.innerHTML = `Page ${pid}`
-      }
-    })
+    windowElement.addEventListener('scroll', handleEndlessScrolling)
     M.toast({ html: 'Enabled : Endless Scrolling' })
   }
 })
+
+function handleEndlessScrolling () {
+  if (windowElement.scrollY >= documentElement.scrollHeight - windowElement.innerHeight - 0.1) {
+    let url = getUrl(tags, imgLimit, rating)
+    pid++
+    getResults(`${url}&pid=${pid}`)
+    displayPid.innerHTML = `Page ${pid}`
+  }
+}
 
 // Online / Offline detection
 if (!navigator.onLine) {
