4d7628761ead1ecf4211fb9c322abe683b0dfd3a
KeziahMoselle <keziahmoselle@protonmail.com>
2018-10-04 20:15:50 +0200

    Add autoUpdater

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 // Modules
 
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const { app, BrowserWindow, dialog } = require('electron')
+const { autoUpdater } = require('electron-updater')
 const path = require('path')
 const url = require('url')
 const store = require('./store')
@@ -39,3 +38,53 @@ app.on('window-all-closed', function () {
     app.quit()
   }
 })
+
+// Auto Updater
+
+let updater
+autoUpdater.autoDownload = false
+
+autoUpdater.on('error', (error) => {
+  dialog.showErrorBox('Error', error === null ? 'Unknown' : (error.stack || error).toString())
+})
+
+autoUpdater.on('update-available', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    title: 'An update is available !',
+    'message': 'Do you want to update now ?',
+    buttons: ['Yes !', 'No']
+  }, (buttonIndex) => {
+    if (buttonIndex === 0) {
+      autoUpdater.downloadUpdate()
+    } else {
+      updater.enabled = true
+      updater = null
+    }
+  })
+})
+
+autoUpdater.on('update-not-available', () => {
+  dialog.showMessageBox({
+    title: 'No Updates',
+    message: 'Current version is up-to-date !'
+  })
+  updater.enabled = true
+  updater = null
+})
+
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    title: 'Install Updates',
+    message: 'Updates downloaded, the client will be quit for update...'
+  }, () => {
+    setImmediate(() => autoUpdater.quitAndInstall())
+  })
+})
+
+function checkForUpdates (menuItem, focusedWindow, event) {
+  updater = menuItem
+  updater.enabled = false
+  autoUpdater.checkForUpdates()
+}
+module.exports.checkForUpdates = checkForUpdates
